d170e606a1387d066e87b1377353e7a780a45926
RyhorPaprauka <poststone.minsk@gmail.com>
2019-02-02 18:00:54 +0300

    HomeWork3

---
+++ src/HW3/Task1.java
@@ -0,0 +1,19 @@
+package HW3;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = n.nextInt();
+        int sum = 0;
+
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        System.out.println(sum);
+    }
+}
